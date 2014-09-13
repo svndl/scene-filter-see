a55445880625420dae17991e860cb1f74c0bc328
leksea <leksea@gmail.com>
2014-09-12 17:05:43 -0700

    Scatter plots updated

--- ImageAnalysis/Scripts/pyrAnalysis.m
+++ ImageAnalysis/Scripts/pyrAnalysis.m
@@ -65,36 +65,36 @@ function pyrAnalysis
             heightS = floor(spyrHt(spindIm));
             
             %arrays for storing correlation values
-            ld_corr_L = zeros(heightW, 1);
-            mean_lum_L = zeros(heightW, 1);
-            sf_L = zeros(heightW, 1);
+            ld_corr_L = NaN(heightL, 1);
+            mean_lum_L = zeros(heightL, 1);
+            sf_L = zeros(heightL, 1);
             
-            ld_corr_W = zeros(heightL, 1);
-            mean_lum_W = zeros(heightL, 1);
-            sf_W = zeros(heightL, 1);
+            ld_corr_W = NaN(heightW, 1);
+            mean_lum_W = zeros(heightW, 1);
+            sf_W = zeros(heightW, 1);
             
-            ld_corr_S = zeros(heightS, 1);
+            ld_corr_S = NaN(heightS, 1);
             mean_lum_S = zeros(heightS, 1);
             sf_S = zeros(heightS, 1);
                        
             %% Original             
             sf_orig = 1/size(im, 2);            
-            ld_corr_orig = corr(im(validIdx), depth);           
+            ld_corr_orig = corr(depth, im(validIdx));           
             mean_lum_orig = mean(mean(normM(im)));
 
             %% Laplacian
             for levL = 1:heightL
                     lum = (reconLpyr(lpyrIm, lpindIm, levL));
                     
-                    ld_corr_L(levL) = corr(lum(validIdx), depth);
-                    mean_lum_L(levL) = mean(mean(normM(levL)));
+                    ld_corr_L(levL) = corr(depth, lum(validIdx));
+                    mean_lum_L(levL) = mean(mean(normM(lum)));
                     sf_L(levL) = 1/lpindIm(levL, 2);
             end
             %% Wavelet
             for levW = 1:heightW
                     lum = reconWpyr(wpyrIm, wpindIm, 'qmf9', 'reflect1', levW, 'all');
 
-                    ld_corr_W(levW) = corr(lum(validIdx), depth);                    
+                    ld_corr_W(levW) = corr(depth, lum(validIdx));                    
                     mean_lum_W(levW) = mean(mean(normM(lum)));
                     sf_W(levW) = 1/size(wpyrBand(wpyrIm, wpindIm, levW), 2);
             end
@@ -102,7 +102,7 @@ function pyrAnalysis
             for levS = 1:heightS
                     lum = reconSpyr(spyrIm, spindIm, 'sp1Filters', 'reflect1', levS, 'all');
                     
-                    ld_corr_S(levS) = corr(lum(validIdx), depth);                    
+                    ld_corr_S(levS) = corr(depth, lum(validIdx));                    
                     mean_lum_S(levS) = mean(mean(normM(lum)));
                     sf_S(levS) = 1/size(spyrBand(spyrIm, spindIm, levS), 2);
             end
@@ -111,30 +111,31 @@ function pyrAnalysis
             f = figure('Name', listing(l).name, 'visible', 'off');
                        
             f1 = subplot(2, 1, 1);
-            title(f1, 'Luminance-depth correlation across different bands');
-            xlabel(f1, 'spatial frequency');
-            ylabel(f1, 'Correlation value');
+            title(f1, 'Luminance-depth correlation at different scales'); hold on;
+            xlabel(f1, 'spatial frequency, fine to coarse'); hold on;
+            ylabel(f1, 'Luminance-depth correlation'); hold on;
             
             validL = ~isnan(ld_corr_L);
             validW = ~isnan(ld_corr_W);
             validS = ~isnan(ld_corr_S);
-            scatter(sf_orig, ld_corr_orig, 'Marker', '^'); hold on;
+            scatter(sf_orig, ld_corr_orig, 'filled', 'Marker', '^'); hold on;
             scatter(sf_L(validL), ld_corr_L(validL), 'Marker', '*'); hold on;
             scatter(sf_W(validW), ld_corr_W(validW), 'Marker', 'o'); hold on;
             scatter(sf_S(validS), ld_corr_S(validS), 'Marker', '+'); hold on;
-            legend('Origional', 'Laplacian', 'Wavelet', 'Steerable');
+            legend('Original', 'Laplacian', 'Wavelet', 'Steerable');
         
                        
             f2 = subplot(2, 1, 2);
-            title(f2, 'Mean luminance across different bands');
-            xlabel(f2, 'spatial frequency');
-            ylabel(f2, 'Mean');
+            title(f2, 'Mean luminance at different scales'); hold on;
+            xlabel(f2, 'spatial frequency, fine to coarse'); hold on;
+            ylabel(f2, 'Mean'); hold on;
 
-            scatter(sf_orig, mean_lum_orig, 'Marker', '^'); hold on;
+            scatter(sf_orig, mean_lum_orig, 'filled', 'Marker', '^'); hold on;
             scatter(sf_L, mean_lum_L, 'Marker', '*'); hold on;
             scatter(sf_W, mean_lum_W, 'Marker', 'o'); hold on;
             scatter(sf_S, mean_lum_S, 'Marker', '+'); hold on;
             legend('Original', 'Laplacian', 'Wavelet', 'Steerable');
+            
             saveas(f, strcat(path_save, listing(l).name), 'jpeg');
             close(f);               
         end
