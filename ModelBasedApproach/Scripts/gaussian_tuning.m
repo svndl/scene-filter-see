a7e10306251d24815bab94cb50fa7f39b54c8059
Alexandra Yakovleva <leksea@gmail.com>
2014-09-15 15:29:16 -0700

    Update gaussian_tuning.m
    Enable vector support for mu

--- ModelBasedApproach/Scripts/gaussian_tuning.m
+++ ModelBasedApproach/Scripts/gaussian_tuning.m
@@ -1,6 +1,12 @@
-function [resp] = gaussian_tuning(u,s,range);
-
-% evaluate gaussian distribution with mean u and sigma s over supplied range
-
-resp = ( 1/( s*sqrt(2*pi) ) )* ...
-    exp( -1/2.* ( ( range - u )./s ) .^2 );
+function resp = gaussian_tuning(mu, s, x)
+    
+    %resp = zeros(length(mu), length(x));
+    % enable vector support
+    % new x will be the 
+    x1 = repmat(x, length(mu), 1);
+    mu1 = repmat(mu, length(x), 1)';
+    % i-th row will be x - mu(i)    
+    % evaluate gaussian distribution with mean u and sigma s over supplied range
+    
+    resp = 1/( s*sqrt(2*pi) )*exp(0.5*((x1 - mu1)./s ).^2);
+end
