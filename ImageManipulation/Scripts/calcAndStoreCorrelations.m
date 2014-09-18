3f3691ca02d72412ae258e0ac3a6e9036f2be7a9
emily <emilyacooper@gmail.com>
2014-09-18 14:32:48 -0700

    removed ut images from correlation data file

--- ImageManipulation/Scripts/calcAndStoreCorrelations.m
+++ ImageManipulation/Scripts/calcAndStoreCorrelations.m
@@ -1,13 +1,16 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function calcAndStoreCorrelations(image)
 
+% for now we exclude ut images
+if isempty(strfind(image.name,'ut_'))
+    
     %store the original and manipulated luminance/depth correlations
     imY = rgb2gray(image.imRGB.^2.2);
     imYnew = rgb2gray(image.imRGBnew.^2.2);
-
+    
     corrOrig = corr(image.imZOrig(~isnan(image.imZOrig)), imY(~isnan(image.imZOrig)));
     corrNew = corr(image.imZOrig(~isnan(image.imZOrig)), imYnew(~isnan(image.imZOrig)));
-
+    
     if exist('luminanceDepthCorrelations.mat', 'file')
         load('luminanceDepthCorrelations.mat');
         ind = find(ismember(corrDat.scene, image.loadpath));
@@ -17,7 +20,7 @@ function calcAndStoreCorrelations(image)
     else
         ind = 1;
     end
-
+    
     corrDat.scene{ind} = image.loadpath;
     corrDat.corrOrig(ind) = corrOrig;
     switch image.flag
@@ -28,9 +31,9 @@ function calcAndStoreCorrelations(image)
         otherwise
             %do nothing yet
     end
-
+    
     save('luminanceDepthCorrelations.mat', 'corrDat')
-
+    
     %store the original and manipulated imag variances
     if exist('luminanceDepthVariances.mat', 'file')
         load('luminanceDepthVariances.mat');
@@ -41,17 +44,19 @@ function calcAndStoreCorrelations(image)
     else
         ind = 1;
     end
-
+    
     varDat.scene{ind} = image.loadpath;
     varDat.varOrig(ind) = var(imY(:));
     switch image.flag
         case 'tp'
-        varDat.varTP(ind) = var(imYnew(:));
+            varDat.varTP(ind) = var(imYnew(:));
         case'ap'
             varDat.varAP(ind) = var(imYnew(:));
         otherwise
-        % do nothing yet    
+            % do nothing yet
     end
-
+    
     save('luminanceDepthVariances.mat', 'varDat');
+end
+
 end
