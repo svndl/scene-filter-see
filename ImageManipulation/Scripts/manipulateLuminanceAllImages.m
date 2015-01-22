a61f60b35a0fe3e327ccef9c10917cd4afe3525a
leksea <leksea@gmail.com>
2015-01-22 11:21:39 -0800

    handling new MB images

--- ImageManipulation/Scripts/manipulateLuminanceAllImages.m
+++ ImageManipulation/Scripts/manipulateLuminanceAllImages.m
@@ -1,11 +1,11 @@
-function manipulateLuminanceAllImages()
+function manipulateLuminanceAllImages
 
 % This function runs the manipulatLuminance function on all scenes
 % contained in the Images/Originals directory
 
 % Emily Cooper, Stanford University 2014
-    mpath = userpath;
-    abs_path = strcat(mpath(1:end - 1), '/scene-filter-see/ImageManipulation/Images/Originals');
+    mpath = strtok(userpath, ':');
+    abs_path = strcat(mpath, '/scene-filter-see/ImageManipulation/Images/Originals');
     rel_path = '../Images/Originals';
 
     %figure out path locations
