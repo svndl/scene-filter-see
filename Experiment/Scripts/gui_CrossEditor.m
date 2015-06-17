8264077588562348f26a6fd7aa0362d5f20d3ad4
Alexandra Yakovleva <leksea@gmail.com>
2015-06-17 15:39:54 -0700

    Update gui_CrossEditor.m
    Added: shifting cross vertically

--- Experiment/Scripts/gui_CrossEditor.m
+++ Experiment/Scripts/gui_CrossEditor.m
@@ -2,7 +2,7 @@ function gui_CrossEditor(varargin)
     close all;
     
     if (nargin == 0)
-        dbType = 'mb';
+        dbType = 'ut';
     else
         dbType = varargin{1};
     end
@@ -26,33 +26,88 @@ function gui_CrossEditor(varargin)
    
     S.pp = uicontrol('style','pop',...
                  'unit','pix',...
-                 'position',[490 10 280 20],...
+                 'position',[350 110 180 20],...
                  'backgroundc',get(S.fh,'color'),...
                  'fontsize',12,'fontweight','bold',... 
                  'string',list_s,'value',default_pos);
-             
-    S.ed = uicontrol('style','edit',...
+    
+    % distance from LEFT edge of the gui
+    text_edit.X0 = 150;
+    % horizontal gap between the next edit field
+    text_edit.dX = 110;
+    % width of edit field 
+    text_edit.X = 60;
+    
+    
+    % distance from BOTTOM of the gui
+    text_edit.Y0 = 30;
+    % vertical gap between the next edit field    
+    text_edit.dY = 10;
+    % height of edit field 
+  
+    text_edit.Y = 30;
+    
+    
+    S.textH = uicontrol('style','text',...
                  'unit','pix',...
-                 'position',[10 60 60 30],...
+                 'position',[130 110 60 30],...
+                 'fontsize',16,...
+                 'string', 'Hor', 'visible', 'on');     
+
+    S.textV = uicontrol('style','text',...
+                 'unit','pix',...
+                 'position',[240 110 60 30],...
+                 'fontsize',16,...
+                 'string', 'Vert', 'visible', 'on');     
+    % Cross shift text and edit         
+    S.textCross = uicontrol('style','text',...
+                 'unit','pix',...
+                 'position',[50 70 60 30],...
+                 'fontsize',16,...
+                 'string', 'Cross', 'visible', 'on');     
+           
+    S.edX = uicontrol('style','edit',...
+                 'unit','pix',...
+                 'position',[130 70 60 30],...
                  'fontsize',16,...
                  'string',num2str(S.currentScene.offset(2)));
     
-    S.edH = uicontrol('style','edit',...
+    S.edY = uicontrol('style','edit',...
                  'unit','pix',...
-                 'position',[130 60 60 30],...
+                 'position',[240 70 60 30],...
+                 'fontsize',16,...
+                 'string',num2str(S.currentScene.offset(1)));
+             
+    %Scene shift and edit        
+    S.textShift = uicontrol('style','text',...
+                 'unit','pix',...
+                 'position',[50 30 60 30],...
+                 'fontsize',16,...
+                 'string', 'Scene', 'visible', 'on');     
+
+             
+    S.edHX = uicontrol('style','edit',...
+                 'unit','pix',...
+                 'position',[130 30 60 30],...
+                 'fontsize',16,...
+                 'string', '0');
+    
+    S.edHY = uicontrol('style','edit',...
+                 'unit','pix',...
+                 'position',[240 30 60 30],...
                  'fontsize',16,...
                  'string','0');
-                
+             
     S.pb = uicontrol('style','push',...
                  'units','pix',...
-                 'position',[10 130 60 30],...
+                 'position',[550 110 60 30],...
                  'backgroundcolor','w',...
                  'HorizontalAlign','left',...
                  'string','Save X',...
                  'fontsize',14,'fontweight','bold');             
     S.text = uicontrol('style','text',...
                  'unit','pix',...
-                 'position',[200 60 160 30],...
+                 'position',[750 110 160 30],...
                  'fontsize',16,...
                  'string', 'Changes saved!', 'visible', 'off');     
      S.left = axes('units','pixels',...
@@ -64,8 +119,11 @@ function gui_CrossEditor(varargin)
     %align([S.left S.right],'distribute','bottom');    
     set(S.pp,'callback',{@pp_call, S});  % Set the popup callback.
     set(S.pb, 'callback', {@pb_call, S});
-    set(S.ed,'call',{@ed_call, S});  % Sharedclose all Callback.
-    set(S.edH,'call',{@edH_call, S});  % Sharedclose all Callback.
+    set(S.edX,'call',{@edX_call, S});  % Sharedclose all Callback.
+    set(S.edY,'call',{@edY_call, S});  % Sharedclose all Callback.
+    set(S.edHX,'call',{@edHX_call, S});  % Sharedclose all Callback.
+    set(S.edHY,'call',{@edHY_call, S});  % Sharedclose all Callback.
+
     updateCrossPos(S);    
 end
 
@@ -74,13 +132,14 @@ function [] = pp_call(varargin)
     S = varargin{3};  % Get the structure.
     P = get(S.pp, 'val'); % Get the users choice from the popup     
     S.currentScene = getScene(S.data, P);
+    
     %update slider/edit info
     updateSceneHandles(S);
     set(S.text, 'visible', 'off')   
     updateCrossPos(S);
 end    
 
-function [] = ed_call(varargin)
+function [] = edX_call(varargin)
     % Callback for the edit box and slider.
     [h, S] = varargin{[1,3]};  % Get calling handle and structure.
     new_offset = str2double(get(h,'string'));
@@ -88,11 +147,29 @@ function [] = ed_call(varargin)
     updateCrossPos(S);
     updateCallbacks(S);    
 end
-function [] = edH_call(varargin)
+
+function [] = edY_call(varargin)
+    % Callback for the edit box and slider.
+    [h, S] = varargin{[1,3]};  % Get calling handle and structure.
+    new_offset = str2double(get(h,'string'));
+    S.currentScene.offset(1) = round(new_offset);
+    updateCrossPos(S);
+    updateCallbacks(S);    
+end
+
+function [] = edHX_call(varargin)
     % Callback for the edit box and slider.
     [h, S] = varargin{[1,3]};  % Get calling handle and structure.
     new_shift = str2double(get(h,'string'));
-    S.currentScene.dH = round(new_shift);
+    S.currentScene.dH(2) = round(new_shift);
+    updateCallbacks(S);    
+end
+
+function [] = edHY_call(varargin)
+    % Callback for the edit box and slider.
+    [h, S] = varargin{[1,3]};  % Get calling handle and structure.
+    new_shift = str2double(get(h,'string'));
+    S.currentScene.dH(1) = round(new_shift);
     updateCallbacks(S);    
 end
 
@@ -109,14 +186,15 @@ function [] = pb_call(varargin)
     path = main_setPath_Experiment;
     save([path.metadata_gui_scenes filesep 'gui_' S.currentScene.name '.mat'], 'name','right', 'left', 'offset', 'dH', 'offset0');
     
-    rx = edit_drawCross(right, 30, -offset);
-    lx = edit_drawCross(left, 30, offset);
+    rx = edit_drawCross(right, 30, [offset(1), - offset(2)]);
+    lx = edit_drawCross(left, 30, [offset(1), offset(2)]);
     d = calc_getDisplay;
-    shiftH = offset(2) + dH;
-    lA = edit_positionScene(lx, d.h, shiftH);
-    rA = edit_positionScene(flipdim(rx, 2), d.h, shiftH);
+    shiftH = offset + dH;
+    lA = edit_positionScene(lx, [d.v, d.h], shiftH, 'hv');
+    rA = edit_positionScene(flipdim(rx, 2), [d.v, d.h], shiftH, 'hv');
    
     sceneA = cat(2, lA, rA);
+    sceneA = sceneA.^(1/2.2);
     imwrite(sceneA, [path.metadata_gui_scenes filesep 'gui_' S.currentScene.name '.jpeg']);
     %write matfiles
     
@@ -124,24 +202,25 @@ function [] = pb_call(varargin)
 end
 
 function updateCrossPos(S)
-    S.currentScene.name
-    S.currentScene.rx = edit_drawCross(S.currentScene.right, 30, -S.currentScene.offset);
-    S.currentScene.lx = edit_drawCross(S.currentScene.left, 30, S.currentScene.offset);
+    S.currentScene.rx = edit_drawCross(S.currentScene.right, 30, [S.currentScene.offset(1), -S.currentScene.offset(2)]);
+    S.currentScene.lx = edit_drawCross(S.currentScene.left, 30, [S.currentScene.offset(1), S.currentScene.offset(2)]);
     midV = floor(0.5*size(S.currentScene.lx, 1));
     midH = floor(0.5*size(S.currentScene.lx, 2));
     midLeft = S.currentScene.lx(midV - 400:midV + 400, midH-400:midH + 400, :);
     midRight = S.currentScene.rx(midV - 400:midV + 400, midH-400:midH + 400, :);
 	
     set(0,'CurrentFigure',S.fh);
-    set(S.fh,'CurrentAxes',S.left); imshow(midLeft);
-    set(S.fh,'CurrentAxes',S.right); imshow(midRight);
+    set(S.fh,'CurrentAxes',S.left); imshow(midLeft.^(1/2.2));
+    set(S.fh,'CurrentAxes',S.right); imshow(midRight.^(1/2.2));
     updateCallbacks(S);
 end
 %will be called when new scene is loaded for the first time
 function updateSceneHandles(S)
     %update edit field value
-    set(S.ed, 'string', num2str(S.currentScene.offset(2)));
-    set(S.edH, 'string', num2str(S.currentScene.dH));   
+    set(S.edX, 'string', num2str(S.currentScene.offset(2)));
+    set(S.edY, 'string', num2str(S.currentScene.offset(1)));
+    set(S.edHX, 'string', num2str(S.currentScene.dHX));   
+    set(S.edHY, 'string', num2str(S.currentScene.dHY));   
     
     updateCallbacks(S);
 end
@@ -150,7 +229,7 @@ function out = getScene(s, pos)
     if (~isfield(scene, 'offset0'))
         out = gui_getScene(scene);
         out.offset = floor(0.5*out.offset0);
-        out.dH = 0;
+        out.dH = [0 0];
     else
        out = scene;
     end
@@ -158,7 +237,9 @@ end
 function updateCallbacks(S)
 % 
     set(S.pp, 'callback', {@pp_call, S});  % update the popup callback.    
-    set(S.ed, 'callback', {@ed_call, S});  % update the edit callback.
-    set(S.edH, 'callback', {@edH_call, S});  % update the edit callback.    
+    set(S.edX, 'callback', {@edX_call, S});  % update the edit callback.
+    set(S.edHX, 'callback', {@edHX_call, S});  % update the edit callback.    
+    set(S.edY, 'callback', {@edY_call, S});  % update the edit callback.
+    set(S.edHY, 'callback', {@edHY_call, S});  % update the edit callback.    
     set(S.pb, 'callback', {@pb_call, S});
 end
